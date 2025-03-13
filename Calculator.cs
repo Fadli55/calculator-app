4bcff9f514019ffe68406146d49b76c531cbc288
Fadli <rizkifadli1908@students.amikom.ac.id>
2025-03-13 10:11:19 +0700

    PERKALIAN

--- Calculator.cs
+++ Calculator.cs
@@ -7,10 +7,10 @@ class Calculator
 		
 		Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}", a, b, Penambahan(a, b));
 		Console.WriteLine("Hasil Pengurangan: {0} - {1} = {2}", a, b, Pengurangan(a, b));
+		Console.WriteLine("Hasil Perkalian: {0} - {1} = {2}", a, b, Perkalian(a, b));
 		
 		Console.WriteLine("\nTokan sembarang key untuk keluar");
 		Console.ReadKey();
-			
 	}
 	
 	static int Penambahan(int a, int b)
@@ -21,5 +21,10 @@ class Calculator
 	static int Pengurangan(int a, int b)
 	{
 		return a - b;
+	}
+	
+	static int Perkalian(int a, int b)
+	{
+		return a * b;
 	}
 }
